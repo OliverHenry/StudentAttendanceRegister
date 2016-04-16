a525a8fa2bac9ac35311234c01e5ed0afe4b7273
Oliver Henry <oliverhenry95@gmail.com>
2016-04-16 20:31:25 +0100

    User Courses window added

---
+++ StudentAttendanceRegister/UserCourses.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace StudentAttendanceRegister
+{
+    partial class UserCourses
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.studentComboBox = new System.Windows.Forms.ComboBox();
+            this.addToCourseButton = new System.Windows.Forms.Button();
+            this.coursesComboBox = new System.Windows.Forms.ComboBox();
+            this.tempCheckBox = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // studentComboBox
+            // 
+            this.studentComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.studentComboBox.FormattingEnabled = true;
+            this.studentComboBox.Location = new System.Drawing.Point(82, 81);
+            this.studentComboBox.Name = "studentComboBox";
+            this.studentComboBox.Size = new System.Drawing.Size(210, 28);
+            this.studentComboBox.TabIndex = 0;
+            this.studentComboBox.SelectedIndexChanged += new System.EventHandler(this.studentComboBox_SelectedIndexChanged);
+            // 
+            // addToCourseButton
+            // 
+            this.addToCourseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addToCourseButton.Location = new System.Drawing.Point(186, 229);
+            this.addToCourseButton.Name = "addToCourseButton";
+            this.addToCourseButton.Size = new System.Drawing.Size(106, 41);
+            this.addToCourseButton.TabIndex = 3;
+            this.addToCourseButton.Text = "Add to Course";
+            this.addToCourseButton.UseVisualStyleBackColor = true;
+            this.addToCourseButton.Click += new System.EventHandler(this.addToCourseButton_Click);
+            // 
+            // coursesComboBox
+            // 
+            this.coursesComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.coursesComboBox.FormattingEnabled = true;
+            this.coursesComboBox.Location = new System.Drawing.Point(82, 129);
+            this.coursesComboBox.Name = "coursesComboBox";
+            this.coursesComboBox.Size = new System.Drawing.Size(210, 28);
+            this.coursesComboBox.TabIndex = 5;
+            // 
+            // tempCheckBox
+            // 
+            this.tempCheckBox.AutoSize = true;
+            this.tempCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tempCheckBox.Location = new System.Drawing.Point(102, 174);
+            this.tempCheckBox.Name = "tempCheckBox";
+            this.tempCheckBox.Size = new System.Drawing.Size(65, 22);
+            this.tempCheckBox.TabIndex = 6;
+            this.tempCheckBox.Text = "Temp";
+            this.tempCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Student";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(2, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Course";
+            // 
+            // UserCourses
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 441);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tempCheckBox);
+            this.Controls.Add(this.coursesComboBox);
+            this.Controls.Add(this.addToCourseButton);
+            this.Controls.Add(this.studentComboBox);
+            this.Name = "UserCourses";
+            this.Text = "User Courses";
+            this.Load += new System.EventHandler(this.UserCourses_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox studentComboBox;
+        private System.Windows.Forms.Button addToCourseButton;
+        private System.Windows.Forms.ComboBox coursesComboBox;
+        private System.Windows.Forms.CheckBox tempCheckBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ StudentAttendanceRegister/UserCourses.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace StudentAttendanceRegister
+{
+    public partial class UserCourses : Form
+    {
+        public UserCourses()
+        {
+            InitializeComponent();
+        }
+
+        private void UserCourses_Load(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.AppSettings["DBConnection"];
+
+            // Provide the query string with a parameter placeholder.
+            string queryString = "SELECT first_name FROM users;";
+
+            // Create and open the connection in a using block. This
+            // ensures that all resources will be closed and disposed
+            // when the code exits.
+            using (SqlConnection connection =
+                new SqlConnection(connectionString))
+            {
+
+                // Create the Command and Parameter objects.
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                reader.Read();
+                string userTypeSQL = "SELECT u.id, u.first_name, u.last_name FROM users u JOIN user_types ut ON u.user_type_id = ut.id AND ut.name = 'Student';";
+                SqlCommand userTypeCommand = new SqlCommand(userTypeSQL, connection);
+                SqlDataReader userTypeReader = userTypeCommand.ExecuteReader();
+
+                try
+                {
+                   while (userTypeReader.Read())
+                    {
+                        studentComboBox.Items.Add(String.Format("({0}) {1} {2}", userTypeReader[0], userTypeReader[1], userTypeReader[2]));
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+        }
+
+        private void studentComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.AppSettings["DBConnection"];
+
+            // Provide the query string with a parameter placeholder.
+            string queryString = "SELECT code FROM courses;";
+
+            // Create and open the connection in a using block. This
+            // ensures that all resources will be closed and disposed
+            // when the code exits.
+            using (SqlConnection connection =
+                new SqlConnection(connectionString))
+            {
+
+                // Create the Command and Parameter objects.
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                reader.Read();
+                string coursesSQL = "SELECT code FROM courses;"; //TODO - Filter out existing courses
+                SqlCommand coursesCommand = new SqlCommand(coursesSQL, connection);
+                SqlDataReader coursesReader = coursesCommand.ExecuteReader();
+                
+                try
+                {
+                    coursesComboBox.Items.Clear();
+                    while (coursesReader.Read())
+                    {
+                        coursesComboBox.Items.Add(String.Format("{0}", coursesReader[0]));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+        }
+
+        private void addToCourseButton_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.AppSettings["DBConnection"]))
+            {
+                string queryString = "INSERT INTO user_courses (user_id, course_id, temp_user) SELECT u.Id user_id, c.Id course_id, @temp_flag temp FROM users u CROSS JOIN courses c WHERE '('+CAST(u.Id AS VARCHAR(50))+') '+u.first_name+' '+u.last_name = @student AND c.code = @code;";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.AddWithValue("@student", studentComboBox.Text);
+                command.Parameters.AddWithValue("@code", coursesComboBox.Text);
+                command.Parameters.AddWithValue("@temp_flag", tempCheckBox.Checked ? 1 : 0);
+
+                //Try to connect to the DB
+                try
+                {
+                    connection.Open();
+                    //Try to run the query
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                        //Console.WriteLine(studentComboBox.Text);
+                        //Console.WriteLine(coursesComboBox.Text);
+                        //Console.WriteLine(tempCheckBox.Checked ? 1 : 0);
+                        MessageBox.Show("Course assigned");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        MessageBox.Show("Student already assigned");
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+                catch (Exception connEx)
+                {
+                    Console.WriteLine(connEx.Message);
+                    MessageBox.Show(connEx.Message);
+                }
+            }
+
+        }
+   }
+}
